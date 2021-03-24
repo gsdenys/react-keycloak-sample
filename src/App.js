a3d194809e53ea861981a1211c4f7dcd827dd295
Denys G. Santos <gsdenys@gmail.com>
2021-03-24 18:12:18 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Demo from './Demo';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App Component</h1>
+      <Demo></Demo>
+    </div>
+  );
+}
+
+export default App;
