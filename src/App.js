6631b076c7b26d39d8f816dd14180a9f8b219c69
Denys G. Santos <gsdenys@gmail.com>
2021-03-24 20:35:19 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import Demo from './Demo';
+import Demo1 from './Demo1';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <h1>App Component</h1>
       <Demo></Demo>
+      <Demo1></Demo1>
     </div>
   );
 }
---
+++ src/Demo1.js
@@ -0,0 +1,19 @@
+import {Component} from 'react';
+
+// function Demo1() {
+//     return (
+//         <div>
+//            <h1>Hello From function Component</h1>
+//         </div>
+//     );
+// }
+
+const Demo1 = () => {
+    return (
+       <div>
+          <h1>Hello From function Component</h1>
+       </div>
+    );
+}
+
+export default Demo1;
